9b1a6c3a1e8db17993c2f02293d93a02d7df4f76
Yan Wei <yanwei20200202@163.com>
2023-10-10 11:22:54 +0800

    init commit

---
+++ metastore/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -z "$HIVE_HOME" ]; then
+  HIVE_HOME="/opt/hive"
+fi
+
+# DB_TYPE values should in <derby|mysql|postgres>
+if [ -z "$DB_TYPE" ]; then
+  DB_TYPE="mysql"
+fi
+
+${HIVE_HOME}/bin/schematool -dbType ${DB_TYPE} -validate
+if [ "$?" -eq "0" ]
+then
+  echo "Validation Success"
+else
+  echo "Failed Validation "
+  ${HIVE_HOME}/bin/schematool -dbType ${DB_TYPE}  -initSchema
+  echo "Init Completed "
+fi
+
+echo "Starting Hive metastore "
+${HIVE_HOME}/bin/start-metastore
