27480a9d0391d06426a765d8203407302ca2a129
nineinfra <ninebigbig@gmail.com>
2024-03-15 14:37:43 +0100

    support kafka

---
+++ kafka/kafkaStart.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+KAFKA_BROKER_ID=${POD_NAME##*-}
+if grep -q "advertised.listeners" "$KAFKA_HOME/conf/server.properties"; then
+  exec "$KAFKA_HOME/bin/kafka-server-start.sh" "$KAFKA_HOME/conf/server.properties" --override broker.id="${KAFKA_BROKER_ID}"
+else
+  ADVERTISED_LISTENERS=$(printf "%s://%s:%d,%s://%s:%d" "${INTERNAL_PORT_NAME}" "${POD_IP}" "${INTERNAL_PORT}" "${EXTERNAL_PORT_NAME}" "${POD_IP}" "${EXTERNAL_PORT}")
+  exec "$KAFKA_HOME/bin/kafka-server-start.sh" "$KAFKA_HOME/conf/server.properties" \
+                --override broker.id="${KAFKA_BROKER_ID}"  \
+                --override advertised.listeners="${ADVERTISED_LISTENERS}"
+fi
