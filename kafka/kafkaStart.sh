1152c3358778dd0be55c55781b132e63000d64e9
nineinfra <ninebigbig@gmail.com>
2024-03-25 15:49:33 +0100

    update dockerfile of kafka and seatunnel

--- kafka/kafkaStart.sh
+++ kafka/kafkaStart.sh
@@ -5,7 +5,9 @@ if grep -q "advertised.listeners" "$KAFKA_HOME/conf/server.properties"; then
   exec "$KAFKA_HOME/bin/kafka-server-start.sh" "$KAFKA_HOME/conf/server.properties" --override broker.id="${KAFKA_BROKER_ID}"
 else
   ADVERTISED_LISTENERS=$(printf "%s://%s:%d,%s://%s:%d" "${INTERNAL_PORT_NAME}" "${POD_IP}" "${INTERNAL_PORT}" "${EXTERNAL_PORT_NAME}" "${POD_IP}" "${EXTERNAL_PORT}")
+  LISTENERS=$(printf "%s://%s:%d,%s://%s:%d" "${INTERNAL_PORT_NAME}" "${POD_IP}" "${INTERNAL_PORT}" "${EXTERNAL_PORT_NAME}" "${POD_IP}" "${EXTERNAL_PORT}")
   exec "$KAFKA_HOME/bin/kafka-server-start.sh" "$KAFKA_HOME/conf/server.properties" \
                 --override broker.id="${KAFKA_BROKER_ID}"  \
-                --override advertised.listeners="${ADVERTISED_LISTENERS}"
+                --override advertised.listeners="${ADVERTISED_LISTENERS}" \
+                --override listeners="${LISTENERS}"
 fi
